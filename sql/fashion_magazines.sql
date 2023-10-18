d51aba69f4affddc609299aa1f3f902faf27ccfa
james kiper <jamesraykiper@hotmail.com>
2023-10-18 19:48:57 -0400

    first commit

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,10 @@
 -- Add your sql here
+Select  custers.customer_name AS 'Customer', 
+ PRINTF('$%.2f', SUM((subscriptions.subscription_lenght * subscriptions.price_per_month))) AS 'Amount Due'
+FROM orders
+JOIN customers 
+ON orders.order_id = customers.customer_id
+JOIN subscriptions
+ON orders.subscription_id = subsciptions.subscription_id
+WHERE order_status = 'unpaid' AND subscriptions.subscription ='Fashion Magazine';
+GROUP BY customers.customer_name;
