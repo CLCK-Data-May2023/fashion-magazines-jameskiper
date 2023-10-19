d339fede105f1bcac0ffe2e67ae2a3c837985e22
james kiper <jamesraykiper@hotmail.com>
2023-10-19 03:28:38 -0400

    last try

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -8,17 +8,9 @@ ON orders.customer_id = customers.customer_id
 JOIN subscriptions
 ON orders.subscription_id = subsciptions.subscription_id
 WHERE order_status = 'unpaid' AND subscriptions.description = 'Fashion Magazine';
-7GROUP BY customers.customer_name;
+GROUP BY customers.customer_name;
 
---SELECT customers.customer_name AS 'Customer',
-	--PRINTF('$%.2f', SUM((subscriptions.subscription_length * subscriptions.price_per_month))) AS 'Amount Due'
---FROM orders
---JOIN customers 
-	--ON orders.customer_id = customers.customer_id
---JOIN subscriptions
-	--ON orders.subscription_id = subscriptions.subscription_id
---WHERE order_status = 'unpaid' AND subscriptions.description = 'Fashion Magazine'
---GROUP by customers.customer_name;
+
 
 
 
