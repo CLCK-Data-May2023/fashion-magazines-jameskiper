79ef6c1a38ec8b12182418c4de3d549422cc9bc0
james kiper <jamesraykiper@hotmail.com>
2023-10-19 16:22:33 -0400

    27

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -6,7 +6,7 @@ FROM orders
 JOIN customers
 ON orders.customer_id = customers.customer_id
 JOIN subscriptions
-ON orders.subscription_id = subsciptions.subscription_id
+ON orders.subscription_id = subscriptions.subscription_id
 WHERE order_status = 'unpaid' AND subscriptions.description = 'Fashion Magazine';
 GROUP BY customers.customer_name;
 
